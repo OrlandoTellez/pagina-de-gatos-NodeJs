477b9d4c8dcdf82bd31c247711fff8db43fd34cc
Orlando <espadachinkinno@gmail.com>
2025-01-08 16:36:34 -0600

    Se mueven algunas los archivos y se estructura de otra forma las carpetas

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import express from "express"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+app.set("views", join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+}
+)
+
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto:", 3000)
+})
