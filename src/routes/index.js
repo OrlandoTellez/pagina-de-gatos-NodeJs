489501e03a51b2dd8a8bb5c763d872b001c26ec8
Orlando <espadachinkinno@gmail.com>
2025-01-10 14:32:01 -0600

    Se agregan props a las tarjetas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,39 @@
 import { Router } from "express";
 
 const router = Router()
+const cards = [
+    {
+        nombre: "Maine Coon 1",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon 2",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon",
+        imagen: "https://via.placeholder.com/300"
+    },
+    {
+        nombre: "Maine Coon",
+        imagen: "https://via.placeholder.com/300"
+    }
+]
 
 router.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {cards})
 })
 
 router.get("/razas", (req, res) => {
