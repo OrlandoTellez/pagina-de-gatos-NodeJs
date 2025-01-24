e2a63053eea64e8b55b86e5f0485d85f82dc6ea1
Orlando <espadachinkinno@gmail.com>
2025-01-24 16:18:59 -0600

    cambios extraños hay en mi

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-import express from "express"
-import { join, dirname } from "path"
-import { fileURLToPath } from "url"
-import router from "./src/routes/index.js"
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import router from "./src/routes/index.js";
 
-const app = express()
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.set("views", join(__dirname, "src/pages"));
+app.set("view engine", "ejs");
 
-app.set("views", join(__dirname, "src/pages"))
-app.set("view engine", "ejs")
+app.use(express.static(join(__dirname, "./src/public")));
 
-app.use(express.static(join(__dirname, "src/public")))
+app.use(router);
 
-app.use(router)
+app.use((req, res) => {
+  res.status(404).send("Página no encontrada");
+});
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor");
+});
 
-app.listen(4000, () => {
-    console.log("Servidor iniciado en: http://localhost:" + 4000)
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+console.log(`Servidor iniciado en: http://localhost:${PORT}`);
+});
